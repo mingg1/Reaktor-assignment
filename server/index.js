774445121ee09895291792ebd60ae81219aeafb8
Minji Choi <comg2843@gmail.com>
2022-05-26 23:50:17 +0300

    added cors middleware

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routers/router.js';
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-//app.use(multer);
+
+app.use(cors());
 app.use('/upload', router);
 app.listen(PORT, () => console.log(`Server listening on ${PORT} :-)`));
