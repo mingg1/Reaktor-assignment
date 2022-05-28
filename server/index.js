5e59d7beececc9acc7821451f48eac561a03ab14
Minji Choi <comg2843@gmail.com>
2022-05-29 01:02:11 +0300

    modified static file directory

--- server/index.js
+++ server/index.js
@@ -7,11 +7,13 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
-app.use(
-  express.static(
-    path.resolve(path.dirname(__filename), '../client/build', 'index.html')
-  )
-);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(cors());
 app.use('/upload', router);
+
+app.get('/*', (_, res) =>
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+);
 app.listen(PORT, () => console.log(`Server listening on ${PORT} :-)`));
