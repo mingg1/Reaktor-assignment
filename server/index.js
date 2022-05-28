aaf78c51d2a4392077a3fece7e8d9df26b2e39d4
Minji Choi <comg2843@gmail.com>
2022-05-28 21:13:38 +0300

    deployment test

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routers/router.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+const __filename = fileURLToPath(import.meta.url);
+app.use(
+  express.static(
+    path.resolve(path.dirname(__filename), '../client/build', 'index.html')
+  )
+);
 app.use(cors());
 app.use('/upload', router);
 app.listen(PORT, () => console.log(`Server listening on ${PORT} :-)`));
